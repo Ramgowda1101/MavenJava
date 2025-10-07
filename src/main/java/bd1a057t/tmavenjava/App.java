67524d873df1960f9ec1e13e07c1e3e8c68ebc63
Ramgowda1101 <yarlagaddaramgowda2005@gmail.com>
2025-10-07 10:06:47 +0530

    Maven-JavaProject

---
+++ src/main/java/bd1a057t/tmavenjava/App.java
@@ -0,0 +1,10 @@
+package bd1a057t.tmavenjava;
+
+/**
+ * Hello world!
+ */
+public class App {
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
---
+++ src/test/java/bd1a057t/tmavenjava/AppTest.java
@@ -0,0 +1,19 @@
+package bd1a057t.tmavenjava;
+
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
+import org.junit.jupiter.api.Test;
+
+/**
+ * Unit test for simple App.
+ */
+public class AppTest {
+
+    /**
+     * Rigorous Test :-)
+     */
+    @Test
+    public void shouldAnswerWithTrue() {
+        assertTrue(true);
+    }
+}
